fc4a3033f30ed568e61a40ff5a3774eff2b42af9
João Pedro <c99xia@outlook.pt>
2026-01-14 18:02:03 +0000

    em resolução

--- Ex2/Cao.cpp
+++ Ex2/Cao.cpp
@@ -1,1 +1,43 @@
 #include "Cao.h"
+
+
+Cao::Cao(std::string nomeAnimalP, std::string dataNascimentoAnimalP, double pesoAnimalP) :
+	Animal(nomeAnimalP, dataNascimentoAnimalP, pesoAnimalP, "Cao")
+{
+
+}
+
+
+std::string Cao::exercitar() {
+	std::ostringstream oss;
+	oss << "corri x quilometros" << "\n";
+	return oss.str();
+}
+
+std::string Cao::comer() {
+	std::ostringstream oss;
+
+
+	double pesoAtual = getPeso();
+	double novoPeso = pesoAtual * 1.05;
+	if (novoPeso <= 20 && novoPeso > 0) {
+		setPeso(novoPeso);
+		oss << "Biscoitos bons. Obrigado" << "\n";
+		return oss.str();
+	}
+	else {
+		oss << "Sobrepeso" << "\n";
+		return oss.str();
+	}
+
+}
+
+std::string Cao::getDescricao() {
+	std::ostringstream oss;
+	oss << "Nome: " << getNome()
+		<< "Data de Nascimento: " << getDataNascimento()
+		<< "Peso do Cao: " << getPeso()
+		<< "Especie: " << getEspecieAnimal();
+
+	return oss.str();
+}
--- Ex2/Gato.cpp
+++ Ex2/Gato.cpp
@@ -1,1 +1,27 @@
 #include "Gato.h"
+
+Gato::Gato(std::string nomeAnimalP, std::string dataNascimentoAnimalP, double pesoAnimalP) :
+	Animal(nomeAnimalP, dataNascimentoAnimalP, pesoAnimalP, "Gato")
+{
+
+}
+
+std::string Gato::exercitar() {
+
+}
+
+std::string Gato::comer() {
+	std::ostringstream oss;
+	oss << "o que, este pate reles novamente?" << "\n";
+	return oss.str();
+}
+
+std::string Gato::getDescricao() {
+	std::ostringstream oss;
+	oss << "Nome: " << getNome()
+		<< "Data de Nascimento: " << getDataNascimento()
+		<< "Peso do Gato: " << getPeso()
+		<< "Especie: " << getEspecieAnimal();
+
+	return oss.str();
+}
